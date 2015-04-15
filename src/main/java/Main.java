a45c9a4e69be160b705f2598a8b100105dfe770c
Josh Harshman <joshgreyhat@gmail.com>
2015-04-15 16:09:12 -0700

    Update to Main and Star Classes
    - Add singleton functionality to star class
    - Stub out tests in Main

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,16 +1,28 @@
 /**
- * Created by Benjamin Daschel on 4/13/15.
+ * Created by voodoo on 4/14/15.
  */
 public class Main {
+     public static void main(String[] args) {
 
-    public static void main(String[] args) {
-        /*
+         /*
         Tree mytree = new BlueSpruce();
         mytree = new Star(mytree);
         mytree = new Ruffles(mytree);
         mytree = new Star(mytree); //this is problematic!
         mytree = new Ruffles(mytree);
-        printtree(mytree);
-         */
+        printTree(mytree);
+          */
+
+         Tree test = new FraserFir();
+         test = new Ruffles(test);
+         test = new BlueBalls(test);
+         test = Star.getInstance(test);
+         test = Star.getInstance(test);
+         printTree(test);
+    }
+
+    //print tree method
+    public static void printTree(Tree mytree) {
+        System.out.println(mytree.getDescription() + " will cost " + mytree.cost());
     }
 }
--- src/main/java/Star.java
+++ src/main/java/Star.java
@@ -3,9 +3,21 @@
  */
 public class Star extends TreeDecorator {
     Tree tree;
-    public Star(Tree tree) {
+    protected static Star instance = null;
+    protected Star(Tree tree) {
         this.tree = tree;
     }
+
+    public static Star getInstance(Tree tree) {
+        if(instance == null) {
+            instance = new Star(tree);
+        }
+        else
+            System.out.println("Can only have once instance of star!");
+
+        return instance;
+    }
+
     public String getDescription() {
         return tree.getDescription() + ", and topped with an awesome star";
     }
